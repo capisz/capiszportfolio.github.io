f6cf3547e8c70c02bb592ac81ec392a95986b875
capisz <ccapizzuto@yahoo.com>
2026-02-21 00:34:56 -0500

    Fix CI build by removing unused Discord icon import

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import "./index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faDiscord,
   faGithub,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
